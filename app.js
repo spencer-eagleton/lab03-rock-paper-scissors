3fceb0cc4e58dcb90b06ad12669ac5b807616aa0
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-10-06 15:09:03 -0700

    declared intial variables and added click event to grab user selection

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 // import functions and grab DOM elements
+// import { getRandomThrow } from './get-random-throw';
+
+
+
+const playButton = document.getElementById('play-button');
+const totalWins = document.getElementById('wins');
+const totalLosses = document.getElementById('losses');
 
 // initialize global state
-
+let wins = 0;
+let losses = 0;
 // set event listeners 
+
+playButton.addEventListener('click', ()=>{
+  const selected = document.querySelector('input[type=radio]:checked');
+  const userMove = selected.value;
+  console.log(userMove);
+   
+
+})
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
---
+++ get-random-throw.js
@@ -0,0 +1,4 @@
+export function getRandomThrow(){
+    return ['rock', 'paper', 'scissors'][Math.floor(Math.random) * 2]
+
+}
