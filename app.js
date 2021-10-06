eef0ed18d367204939f3926e4b0e924ecf5fbc5d
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-10-06 16:49:11 -0700

    compared user move to computer move and updated state with results. it works!

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 // import functions and grab DOM elements
-import { getRandomThrow } from './get-random-throw.js';
+import { getRandomThrow, didUserWin } from './get-random-throw.js';
 
 
 
 const playButton = document.getElementById('play-button');
 const totalWins = document.getElementById('wins');
 const totalLosses = document.getElementById('losses');
+const totalDraws = document.getElementById('draws');
+const resultDisplay = document.getElementById('result-display');
 
 // initialize global state
 let wins = 0;
 let losses = 0;
+let draws = 0;
 // set event listeners 
 
 playButton.addEventListener('click', ()=>{
@@ -19,7 +22,25 @@ playButton.addEventListener('click', ()=>{
   
   const compMove = getRandomThrow();
   console.log(compMove)
-   
+  
+  const result = didUserWin(userMove, compMove);
+  console.log(result)
+
+  if (result === 1) {
+    resultDisplay.textContent = 'You\'re a winner!';
+    wins++;
+    totalWins.textContent = wins;
+    
+  } else if (result === -1) {
+    resultDisplay.textContent = 'You\'re a LOOOSER!';
+    losses++;
+    totalLosses.textContent = losses;
+
+  } else {
+    resultDisplay.textContent = 'It\'s a draw, play again!';
+    draws++;
+    totalDraws.textContent = draws;
+  }
 
 })
   // get user input
--- get-random-throw.js
+++ get-random-throw.js
@@ -2,3 +2,24 @@ export function getRandomThrow(){
     return ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)]
 
 }
+
+export function didUserWin(userMove, compMove){
+    if (userMove === compMove) {
+        return 0;
+    } else if (userMove === 'rock' && compMove === 'scissors'){
+        return 1;
+    } else if (userMove === 'paper' && compMove === 'rock'){
+        return 1;
+    } else if (userMove === 'scissors' && compMove === 'paper'){
+        return 1;
+    }
+    
+    
+    
+    
+    
+    else {
+        return -1
+    }
+
+}
